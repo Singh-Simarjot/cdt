070ed6bc6671273aa5080519617bbcc88d7c2f63
Simarjot Singh <simarjot.singh@millipixels.com>
2020-03-03 10:58:11 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import React from "react";
 import "./App.css";
 import "./assets/scss/common.scss";
 import "react-dropzone-uploader/dist/styles.css";
-
 // import { Route } from "react-router-dom";
-
 import Header from "./containers/header/header";
 import Dashboard from "./pages/dashboard";
 import AddNewProject from "./pages/addNewProject";
 import ProjectDetail from "./pages/projectDetail";
 // import Home from "./preview/templates/Home";
 import { Switch, Route, Redirect } from "react-router-dom";
-
-import Theme from "./preview/components/Content/Content.jsx";
-import Content from "./preview/components/Content/Content";
-
 import { ProjectsContext } from "./context/projectsContext";
 import Preview from "./preview/index";
 import EditProject from "./pages/editProject";
-
 import Login from "./components/account/login";
-
 import SignUp from "./components/account/signup";
 import ForgotPassword from "./components/account/forgotpassword";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 localStorage.setItem("page", "123");
-
- 
 
 const data = localStorage.getItem("page");
 
